4ed04258a2375dbfb7f3de44e4cd740267bad965
PiotrusWatson <piotruswatson@gmail.com>
2018-03-24 06:23:48 +0000

    added an inventory so that players store any items they absorb into their body

--- Assets/_Scripts/Misc/GrabObjects.cs
+++ Assets/_Scripts/Misc/GrabObjects.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class GrabObjects : MonoBehaviour {
 
-	FixedJoint2D joint;
-	// Use this for initialization
-	void Awake () {
-		joint = GetComponent<FixedJoint2D> ();
-	}
-	
+
 	// Update is called once per frame
 	void OnTriggerEnter2D(Collider2D c){
 		Rigidbody2D otherRigidbody = c.gameObject.GetComponent<Rigidbody2D> ();
 		if (c.gameObject.CompareTag ("Grabbable") && otherRigidbody != null) {
 			c.gameObject.GetComponent<Collider2D> ().isTrigger = true;
-			joint.connectedBody = otherRigidbody;
+			c.transform.SetParent (transform);	
 
 
 		}
---
+++ Assets/_Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour {
+
+	[SerializeField]
+	List<GameObject> inventory;
+	// Use this for initialization
+	void Awake () {
+		inventory = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	public void add(GameObject item){
+		inventory.Add (item);
+	}
+		
+}
