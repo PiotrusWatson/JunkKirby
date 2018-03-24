f461131974054d0d719b3a315b82de7d229ac48b
PiotrusWatson <piotruswatson@gmail.com>
2018-03-24 01:19:54 +0000

    added player center to be wheel

---
+++ Assets/_Scripts/Player/ClampPivotRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClampPivotRotation : MonoBehaviour {
+	public float maxrot = 30f;
+	public float minrot = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		float rotz = transform.rotation.z;
+		rotz = Mathf.Clamp (rotz, 0, 30);
+		transform.localEulerAngles = new Vector3 (transform.localEulerAngles.x, transform.localEulerAngles.y, rotz);
+	}
+}
