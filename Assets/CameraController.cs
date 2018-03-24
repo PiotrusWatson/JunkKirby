844be996cfb0389b8357730fbe55b1e89c447402
PiotrusWatson <piotruswatson@gmail.com>
2018-03-24 02:39:21 +0000

    made camera follow player

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-
+	Vector3 playerPos;
+	[SerializeField]
+	float speed = 5;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void LateUpdate () {
+		playerPos = GameObject.FindGameObjectWithTag ("Player").transform.GetChild (1).localPosition;
+		Vector3 newPos = new Vector3 (playerPos.x, playerPos.y, -10f);
+		transform.position = Vector3.Lerp (transform.position, newPos, speed * Time.fixedDeltaTime);
 	}
 }
