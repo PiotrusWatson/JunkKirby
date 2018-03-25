794bbce2868e382f1aeefaae8ba2865576c66093
PiotrusWatson <piotruswatson@gmail.com>
2018-03-25 11:29:31 +0100

    added spawner

---
+++ Assets/JunkSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunkSpawner : MonoBehaviour {
+	[SerializeField]
+	float spawnTime = 3f;
+	[SerializeField]
+	GameObject[] junkList;
+
+	float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer >= spawnTime) {
+			spawnJunk ();
+			timer = 0f;
+		}
+
+		timer += Time.fixedDeltaTime;
+	}
+
+	void spawnJunk(){
+		int index = Random.Range (0, junkList.Length);
+		Instantiate (junkList [index], transform.position, Quaternion.identity);
+	}
+}
